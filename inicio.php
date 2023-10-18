d33f6899fe4c6342076ce47c446c574967bc8b16
Marwinsoares <marwinsoares070@gmail.com>
2023-10-18 19:32:53 -0300

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -59,9 +59,9 @@
       </div>
 
   <div class="row">
-<div class="col-md-4 div"></div>
-<div class="col-md-4 div"></div>
-<div class="col-md-4 div"></div>
+<div class="col-md-4 div">1</div>
+<div class="col-md-4 div">2</div>
+<div class="col-md-4 div">3</div>
 <div class="col-md-4 div"></div>
 <div class="col-md-4 div"></div>
 <div class="col-md-4 div"></div>
