4d589a52e4fa2a48a75a087d87b3f906a06809cf
Matheusmcs <146785781+Matheusmcs@users.noreply.github.com>
2023-10-21 19:53:18 -0300

    b

--- pedidos.php
+++ pedidos.php
@@ -11,42 +11,47 @@
 
 <body class="body1">
 
-<script>
-    // Obtém os elementos HTML
-    const incrementarBtn = document.querySelector(".incrementar");
-    const decrementarBtn = document.querySelector(".decrementar");
-    const quantidadeNumero = document.querySelector(".quantidade-numero");
-    const precoItem = document.querySelector(".item-preco");
+  <script>
+    // Obtém os botões de incrementar e decrementar
+    const incrementarBotoes = document.querySelectorAll(".incrementar");
+    const decrementarBotoes = document.querySelectorAll(".decrementar");
+
+    // Obtém os elementos relacionados à quantidade e ao preço
+    const quantidadeNumeros = document.querySelectorAll(".quantidade-numero");
+    const precoItens = document.querySelectorAll(".item-preco");
     const precoTotal = document.querySelector(".preco-total");
 
     let quantidade = 0;
     const precoPorItem = 10.00;
 
-    // Função para incrementar a quantidade
-    incrementarBtn.addEventListener("click", function() {
-        quantidade++;
-        quantidadeNumero.innerText = quantidade;
-        atualizarTotal();
-    });
-
-    // Função para decrementar a quantidade
-    decrementarBtn.addEventListener("click", function() {
-        if (quantidade > 0) {
-            quantidade--;
-            quantidadeNumero.innerText = quantidade;
-            atualizarTotal();
-        }
-    });
-
     // Função para atualizar o total
     function atualizarTotal() {
-        const total = quantidade * precoPorItem;
-        precoTotal.innerText = "R$ " + total.toFixed(2);
+      let total = 0;
+      for (let i = 0; i < quantidadeNumeros.length; i++) {
+        total += parseInt(quantidadeNumeros[i].innerText) * precoPorItem;
+      }
+      precoTotal.innerText = "R$ " + total.toFixed(2);
     }
 
-    // Inicializa o total
-    atualizarTotal();
-</script>
+    // Adiciona um ouvinte de eventos para cada botão de incrementar
+    incrementarBotoes.forEach((botao, index) => {
+      botao.addEventListener("click", () => {
+        quantidadeNumeros[index].innerText = parseInt(quantidadeNumeros[index].innerText) + 1;
+        atualizarTotal();
+      });
+    });
+
+    // Adiciona um ouvinte de eventos para cada botão de decrementar
+    decrementarBotoes.forEach((botao, index) => {
+      botao.addEventListener("click", () => {
+        if (parseInt(quantidadeNumeros[index].innerText) > 0) {
+          quantidadeNumeros[index].innerText = parseInt(quantidadeNumeros[index].innerText) - 1;
+          atualizarTotal();
+        }
+      });
+    });
+  </script>
+
 
 
   <header>
@@ -62,8 +67,7 @@
 
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav ">
-            <a class="nav-link1 dropdown-toggle a-dropdown btn btn-outline-light ml-4 btn-warning" href="#"
-              role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link1 dropdown-toggle a-dropdown btn btn-outline-light ml-4 btn-warning" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Perfil
             </a>
             <ul class="dropdown-menu">
@@ -93,13 +97,11 @@
           <img src="imagens/3.png" class="d-block w-100" alt="...">
         </div>
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-        data-bs-slide="prev">
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-        data-bs-slide="next">
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
@@ -144,77 +146,77 @@
         </div>
       </div>
       <div class="col-md-4 div"> <!--3-->
-      <div class="div-pedido">
-        <img src="imagens/pizza_marguerita.png" class="img-fluid2" alt="pizza-calabresa">
-        <h2 class="h2-pedidos">pizza-calabresa</h2>
-        <p class="p-pedidos">pizza mais amada do brasil, calabresa, cebola, molho de tomate, musareala.</p>
-        <div class="pedido">
-          <span class="item-nome">Item do Pedido</span>
-          <span class="item-preco">R$ 10.00</span>
-          <div class="quantidade">
-            <button class="decrementar">-</button>
-            <span class="quantidade-numero">0</span>
-            <button class="incrementar">+</button>
+        <div class="div-pedido">
+          <img src="imagens/pizza_marguerita.png" class="img-fluid2" alt="pizza-calabresa">
+          <h2 class="h2-pedidos">pizza-calabresa</h2>
+          <p class="p-pedidos">pizza mais amada do brasil, calabresa, cebola, molho de tomate, musareala.</p>
+          <div class="pedido">
+            <span class="item-nome">Item do Pedido</span>
+            <span class="item-preco">R$ 10.00</span>
+            <div class="quantidade">
+              <button class="decrementar">-</button>
+              <span class="quantidade-numero">0</span>
+              <button class="incrementar">+</button>
+            </div>
           </div>
-        </div>
 
-      </div>
+        </div>
       </div>
       <div class="col-md-4 div"> <!--4-->
-      <div class="div-pedido">
-        <img src="imagens/pizza_portuguesa.png" class="img-fluid2" alt="pizza-calabresa">
-        <h2 class="h2-pedidos">pizza-calabresa</h2>
-        <p class="p-pedidos">pizza mais amada do brasil, calabresa, cebola, molho de tomate, musareala.</p>
-        <div class="pedido">
-          <span class="item-nome">Item do Pedido</span>
-          <span class="item-preco">R$ 10.00</span>
-          <div class="quantidade">
-            <button class="decrementar">-</button>
-            <span class="quantidade-numero">0</span>
-            <button class="incrementar">+</button>
+        <div class="div-pedido">
+          <img src="imagens/pizza_portuguesa.png" class="img-fluid2" alt="pizza-calabresa">
+          <h2 class="h2-pedidos">pizza-calabresa</h2>
+          <p class="p-pedidos">pizza mais amada do brasil, calabresa, cebola, molho de tomate, musareala.</p>
+          <div class="pedido">
+            <span class="item-nome">Item do Pedido</span>
+            <span class="item-preco">R$ 10.00</span>
+            <div class="quantidade">
+              <button class="decrementar">-</button>
+              <span class="quantidade-numero">0</span>
+              <button class="incrementar">+</button>
+            </div>
           </div>
-        </div>
 
-      </div>
+        </div>
       </div>
       <div class="col-md-4 div"> <!--5-->
-      <div class="div-pedido">
-        <img src="imagens/pizza_gigante_chocolate.png" class="img-fluid2" alt="pizza-calabresa">
-        <h2 class="h2-pedidos">pizza-calabresa</h2>
-        <p class="p-pedidos">pizza mais amada do brasil, calabresa, cebola, molho de tomate, musareala.</p>
-        <div class="pedido">
-          <span class="item-nome">Item do Pedido</span>
-          <span class="item-preco">R$ 10.00</span>
-          <div class="quantidade">
-            <button class="decrementar">-</button>
-            <span class="quantidade-numero">0</span>
-            <button class="incrementar">+</button>
+        <div class="div-pedido">
+          <img src="imagens/pizza_gigante_chocolate.png" class="img-fluid2" alt="pizza-calabresa">
+          <h2 class="h2-pedidos">pizza-calabresa</h2>
+          <p class="p-pedidos">pizza mais amada do brasil, calabresa, cebola, molho de tomate, musareala.</p>
+          <div class="pedido">
+            <span class="item-nome">Item do Pedido</span>
+            <span class="item-preco">R$ 10.00</span>
+            <div class="quantidade">
+              <button class="decrementar">-</button>
+              <span class="quantidade-numero">0</span>
+              <button class="incrementar">+</button>
+            </div>
           </div>
-        </div>
 
-      </div>
+        </div>
       </div>
       <div class="col-md-4 div"> <!--6-->
-      <div class="div-pedido">
-        <img src="imagens/pizza_frango_requeijao.png" class="img-fluid2" alt="pizza-calabresa">
-        <h2 class="h2-pedidos">pizza-calabresa</h2>
-        <p class="p-pedidos">pizza mais amada do brasil, calabresa, cebola, molho de tomate, musareala.</p>
-        <div class="pedido">
-          <span class="item-nome">Item do Pedido</span>
-          <span class="item-preco">R$ 10.00</span>
-          <div class="quantidade">
-            <button class="decrementar">-</button>
-            <span class="quantidade-numero">0</span>
-            <button class="incrementar">+</button>
+        <div class="div-pedido">
+          <img src="imagens/pizza_frango_requeijao.png" class="img-fluid2" alt="pizza-calabresa">
+          <h2 class="h2-pedidos">pizza-calabresa</h2>
+          <p class="p-pedidos">pizza mais amada do brasil, calabresa, cebola, molho de tomate, musareala.</p>
+          <div class="pedido">
+            <span class="item-nome">Item do Pedido</span>
+            <span class="item-preco">R$ 10.00</span>
+            <div class="quantidade">
+              <button class="decrementar">-</button>
+              <span class="quantidade-numero">0</span>
+              <button class="incrementar">+</button>
+            </div>
           </div>
-        </div>
 
+        </div>
       </div>
     </div>
-    </div>
-  
-    
-      
+
+
+
   </main>
 
 
@@ -243,9 +245,7 @@
 
 
 </body>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </html>
