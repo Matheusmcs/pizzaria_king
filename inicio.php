6d72a91e71f4f712b10d676b9d4ee8f373d5d7f7
Matheusmcs <146785781+Matheusmcs@users.noreply.github.com>
2023-10-22 19:23:37 -0300

    s

--- inicio.php
+++ inicio.php
@@ -4,11 +4,19 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< Updated upstream
   <title>Document</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
 <body class="body3">
+=======
+  <title></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="body2">
+>>>>>>> Stashed changes
   <header>
 
     <nav class="navbar navbar-expand-lg nav">
@@ -24,10 +32,15 @@
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
             <ul class="navbar-nav ml-auto">
+<<<<<<< Updated upstream
               <li class="nav-item"><a class="btn btn-outline-light ml-4 btn-warning a-inicio"
                   href="login.php">Entrar</a></li>
               <li class="nav-item"><a class="btn btn-outline-light ml-4 btn-warning a-inicio"
                   href="cadastrar.php">cadastrar</a></li>
+=======
+              <li class="nav-item"><a class="btn btn-outline-light ml-4 btn-warning a-inicio" href="login.php">Entrar</a></li>
+              <li class="nav-item"><a class="btn btn-outline-light ml-4 btn-warning a-inicio" href="cadastrar.php">cadastrar</a></li>
+>>>>>>> Stashed changes
             </ul>
           </div>
         </div>
@@ -41,6 +54,7 @@
     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
+<<<<<<< Updated upstream
           <img src="imagens/banner_pizza.png" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
@@ -57,10 +71,27 @@
       </button>
       <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
         data-bs-slide="next">
+=======
+          <a href="pedidos.php"><img src="imagens/banner pizza06.png" class="d-block w-100" alt="..."></a>
+        </div>
+        <div class="carousel-item">
+          <a href="pedidos.php"><img src="imagens/2.png" class="d-block w-100" alt="..."></a>
+        </div>
+        <div class="carousel-item">
+          <a href="pedidos.php"><img src="imagens/3.png" class="d-block w-100" alt="..."></a>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+>>>>>>> Stashed changes
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
     </div>
+<<<<<<< Updated upstream
   </div>
   <div>
     <h2 class="p-inicio">SOBRE NÓS</h2>
@@ -89,5 +120,12 @@
   integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+=======
+
+  </div>
+</body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+>>>>>>> Stashed changes
 
 </html>
