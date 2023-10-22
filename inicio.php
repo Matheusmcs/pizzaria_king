caad09820e4c3dbf8cf1aa073bedba820fffedd2
Matheusmcs <146785781+Matheusmcs@users.noreply.github.com>
2023-10-22 19:23:12 -0300

    s

---
+++ inicio.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="body3">
+  <header>
+
+    <nav class="navbar navbar-expand-lg nav">
+
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <a class="a-logo" href="inicio.html">
+            <h1 class="logo">Pizzaria <strong id="grosso">King</strong></h1>
+            <!--a logo que eu transformei em letras porque não estou conseguindo colocar as imagens transparentes-->
+          </a>
+        </a>
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item"><a class="btn btn-outline-light ml-4 btn-warning a-inicio"
+                  href="login.php">Entrar</a></li>
+              <li class="nav-item"><a class="btn btn-outline-light ml-4 btn-warning a-inicio"
+                  href="cadastrar.php">cadastrar</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    </nav>
+
+  </header>
+  <div class="div-inicio">
+
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="imagens/banner_pizza.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="imagens/5.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="imagens/4.png" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+  <div>
+    <h2 class="p-inicio">SOBRE NÓS</h2>
+    <p class="p-inicio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ducimus ipsa voluptas in
+      praesentium perferendis saepe aliquid officiis nihil, facere impedit soluta nobis magnam eos natus. Minus, natus
+      praesentium?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quam, incidunt vero ducimus delectus, repellat vitae,
+      cupiditate aperiam unde nostrum culpa. Perferendis autem vel dolorem, neque iure sunt labore distinctio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores aut nobis illum itaque saepe officiis qui
+      vitae voluptas impedit labore, nisi voluptates provident reprehenderit dolores! Sequi debitis perferendis nesciunt
+      dicta.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id sunt, quia quisquam veniam dolorum aliquam quaerat
+      ipsum distinctio hic quo saepe illo cum voluptates ab eum quas consequuntur rem aperiam!
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium dolorem corporis debitis esse, similique,
+      repellat ipsa sed aperiam facere minus mollitia consequuntur voluptatum laudantium maiores totam placeat aliquam
+      quae deserunt.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure ullam amet quod corporis ut aspernatur, dolorem
+      iste ipsam. At similique quo optio molestiae qui rerum dolorem asperiores non debitis rem!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nulla natus ut voluptatum dignissimos modi
+      perspiciatis neque maiores, illo sed quos libero? Mollitia a magni, voluptate obcaecati optio libero
+      necessitatibus.
+    </p>
+  </div>
+</body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</html>
